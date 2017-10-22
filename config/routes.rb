badd9e09ad5af5e1ad53b1c516be323a1287bf1e
pokotsun <nakagawa@triart.co.jp>
2017-10-22 22:39:55 +0900

    topicsのroutes整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'topics/index'
-  get 'topics/show'
-  get 'topics/create'
-
+  # devise用のroot
   devise_for :users, controllers: {
-  confirmations: 'users/confirmations',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations',
-  sessions:      'users/sessions',
-}
+    confirmations: 'users/confirmations',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+  }
 
   root 'static_pages#start'
   get 'static_pages/home'
 
   resources :search
   resources :topics
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
