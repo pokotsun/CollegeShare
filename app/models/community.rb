f58f459c2a307d672603f44d5d74443be94edb1a
pokotsun <nakagawa@triart.co.jp>
2017-10-24 14:43:49 +0900

    search_tableでユーザーのお気に入り情報が得られるようにした

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,12 +2,8 @@ class Community < ApplicationRecord
   has_many :topics, dependent: :destroy
   has_many :community_users
   has_many :users, through: :community_users
-
-  def topic_fav_total
-    num = 0
-    topics.each do |topic|
-      num += topic.good_num
-    end
-    return num
+  
+  def favo_user_num
+    users.count
   end
 end
