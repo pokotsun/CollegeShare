6be6760625c08bf2ab4685007060e38f7f36dd73
pokotsun <nakagawa@triart.co.jp>
2017-10-25 19:00:39 +0900

    assets.rbの修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( users/* )
-# Rails.application.config.assets.precompile += %w( users/registrations )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
