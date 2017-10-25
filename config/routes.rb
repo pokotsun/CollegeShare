a10f48a426b6a97a75fd81b2a558cfac77367d05
pokotsun <nakagawa@triart.co.jp>
2017-10-25 20:04:44 +0900

    GroupController作成

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   resources :search
   resources :topics
+  resources :groups
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
