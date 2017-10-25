e6ba713c071e71b6314751d159bd308936c4aad0
pokotsun <nakagawa@triart.co.jp>
2017-10-25 16:33:54 +0900

    search_tableで表示するデータをすべて実データに入れ替え

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,7 +2,8 @@ class Community < ApplicationRecord
   has_many :topics, dependent: :destroy
   has_many :community_users
   has_many :users, through: :community_users
-  
+  belongs_to :prefecture
+
   def favo_user_num
     users.count
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024042156) do
+ActiveRecord::Schema.define(version: 20171024164002) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 20171024042156) do
     t.integer "sender_id"
     t.integer "receiver_id"
     t.boolean "confirm_flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
