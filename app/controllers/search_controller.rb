29bc7a0b457ad4540f96b744e9aefc1e315b85fc
pokotsun <nakagawa@triart.co.jp>
2017-10-25 18:09:33 +0900

    ページネーション実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     p current_user.inspect
-    @communities = Community.all.order(:prefecture_id)
+    @communities = Community.all.page(params[:page]).per(10).order(:prefecture_id)
   end
 end
