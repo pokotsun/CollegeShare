a7f7b2c56ff49d9395f3bad44e63abb2a95c58df
meta713 <mhdomodomo@gmail.com>
2017-10-27 19:56:22 +0900

    topic調整途中

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,18 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.all.page(params[:page]).per(10).order(:id)
+    @topic = Topic.new
   end
 
   def show
   end
 
   def create
+    @topic = Topic.new
   end
+
+  private
+    def topic_param
+      params.require(:topic).permit(:title, :content, current_user,)
+    end
 end
