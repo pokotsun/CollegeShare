9f39b2fc6cd882593deaf7b070435dd29c9424cd
pokotsun <nakagawa@triart.co.jp>
2017-10-28 19:26:42 +0900

    CarrearWave一旦撤退

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,6 @@ class User < ApplicationRecord
   has_many :group_users
   has_many :groups, through: :group_users
 
-  # 画像アップローダーをマウント
-  mount_uploader :profile_img, ProfileImgUploader
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
