58ce6940a80984fca187c1450e4ddef256a2d97f
pokotsun <nakagawa@triart.co.jp>
2017-10-29 11:59:27 +0900

    グループ作成の内部実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,26 @@ class GroupsController < ApplicationController
   def index
   end
 
+  def create
+    # エラー処理
+    created_group = Group.create(post_params)
+    members = params[:members]
+    group_id = created_group.id
+    members.each do |user_id|
+      GroupUser.create(group_id: group_id,user_id: user_id)
+    end
+
+    redirect_to controller: :channels, action: :index, group_id: group_id
+  end
+
   def show
+  end
+
+  private
+  def post_params
+    # submitしたデータのうち、Model作成に必要なものを
+    # permitの引数に指定する
+    params.require(:group).permit(
+    :name)
   end
 end
